a799cbaa89b3b57b37d2ee9a72396ba5dea27ee8
Filip <filipgolyszny@gmail.com>
2024-04-17 22:39:54 +0200

    Quartz sync: Apr 17, 2024, 10:39 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -22,7 +22,7 @@ const config: QuartzConfig = {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "Caveat",
+        header: "Sedan",
         body: "Source Sans Pro",
         code: "IBM Plex Mono",
       },
